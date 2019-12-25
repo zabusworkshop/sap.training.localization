9aba9f8de69309df31b69701834f3fc6dc1e5528
zabusworkshop <eugeny.zabus@gmail.com>
2019-12-25 07:42:35 +0000

    initial state

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller", "sap/ui/model/resource/ResourceModel"
+], function(Controller, ResourceModel) {
+	"use strict";
+
+	return Controller.extend("sap.training.controller.Main", {
+
+		onInit: function() {
+			
+		},
+
+		onRatingChange: function(oEvent) {
+
+		}
+
+	});
+});
